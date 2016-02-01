91ee250f20f13bf911ad89cd00d1f4ddc6288364
Mina Asham <mina.asham@hotmail.com>
2016-02-01 23:42:59 +0000

    Add comments for XBox360GamepadState enum

--- XBox360ControllerManager/XBox360GamepadState.cs
+++ XBox360ControllerManager/XBox360GamepadState.cs
@@ -1,8 +1,18 @@
 ﻿namespace XBox360ControllerManager
 {
+    /// <summary>
+    /// Represents the state of the gamepad when <see cref="XBox360.GetGamepad"/> is called
+    /// </summary>
     public enum XBox360GamepadState : ushort
     {
+        /// <summary>
+        /// Gamepad is currently connected
+        /// </summary>
         Connected = 0x0000,
+
+        /// <summary>
+        /// Gamepad is not currently connected
+        /// </summary>
         NotConnected = 0x048F
     }
 }
