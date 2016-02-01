c0d81a4dfa251f4c6e62d117c86a72821e15643b
Mina Asham <mina.asham@hotmail.com>
2016-02-01 23:43:52 +0000

    Add comments for XBox360PlayerIndex enum

--- XBox360ControllerManager/XBox360PlayerIndex.cs
+++ XBox360ControllerManager/XBox360PlayerIndex.cs
@@ -1,10 +1,28 @@
 ﻿namespace XBox360ControllerManager
 {
+    /// <summary>
+    /// Represent player/gamepad index
+    /// </summary>
     public enum XBox360PlayerIndex : ushort
     {
+        /// <summary>
+        /// Player one
+        /// </summary>
         One = 0,
+
+        /// <summary>
+        /// Player two
+        /// </summary>
         Two = 1,
+
+        /// <summary>
+        /// Player three
+        /// </summary>
         Three = 2,
+
+        /// <summary>
+        /// Player four
+        /// </summary>
         Four = 3
     }
 }
